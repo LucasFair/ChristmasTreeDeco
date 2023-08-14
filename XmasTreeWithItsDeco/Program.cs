2c4a9352a107cbf10839aac4041f944246ac2fef
LucasFair <141710445+LucasFair@users.noreply.github.com>
2023-08-14 21:00:17 +0200

    Temp upload.

---
+++ XmasTreeWithItsDeco/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace XmasTreeWithItsDeco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string testStr = "*";
+            int testInt = 1;
+            int maxArray = 15;
+            int maxHeight = 8;
+            int[,] testArray = new int[1, maxArray];
+           
+            for (int cnt = 0;  cnt < maxHeight; cnt++)
+            {
+                Console.WriteLine(testStr);
+                for (int width = 0; width < cnt ;)
+                {
+                    width++;
+                    Console.Write("*");
+                }  
+            }
+            */
+
+            /*
+            string[] starArray = {"*",
+                                "* * *",
+                              "* * * * *",
+                            "* * * * * * *",
+                          "* * * * * * * * *",
+                        "* * * * * * * * * * *",
+                      "* * * * * * * * * * * * *",
+                    "* * * * * * * * * * * * * * *"};
+            */
+
+/*
+            int sizeLen = 30;
+            string star_0, star_1, star_2, star_3, star_4, star_5, star_6, star_7;
+            star_0 = "*";
+            star_1 = "* * *";
+            star_2 = "* * * * *";
+            star_3 = "* * * * * * *";
+            star_4 = "* * * * * * * * *";
+            star_5 = "* * * * * * * * * * *";
+            star_6 = "* * * * * * * * * * * * *";
+            star_7 = "* * * * * * * * * * * * * * *";
+
+            // Calculating the string lengths.
+            int star_0_Len, star_1_Len, star_2_Len, star_3_Len, star_4_Len,
+                star_5_Len, star_6_Len, star_7_Len;
+            star_0_Len = star_0.Length;
+            star_1_Len = star_1.Length;
+            star_2_Len = star_2.Length;
+            star_3_Len = star_3.Length;
+            star_4_Len = star_4.Length;
+            star_5_Len = star_5.Length;
+            star_6_Len = star_6.Length;
+            star_7_Len = star_7.Length;
+
+            // Automatically adjusting the spacing according to the string length.
+            var spaceStar_0 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_0_Len/2));
+            var spaceStar_1 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_1_Len/2));
+            var spaceStar_2 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_2_Len/2));
+            var spaceStar_3 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_3_Len/2));
+            var spaceStar_4 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_4_Len/2));
+            var spaceStar_5 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_5_Len/2));
+            var spaceStar_6 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_6_Len/2));
+            var spaceStar_7 = String.Concat(Enumerable.Repeat(" ", sizeLen - star_7_Len/2));
+
+            Console.WriteLine(spaceStar_0 + star_0);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(spaceStar_1 + star_1);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(spaceStar_2 + star_2);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(spaceStar_3 + star_3);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(spaceStar_4 + star_4);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(spaceStar_5 + star_5);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(spaceStar_6 + star_6);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(spaceStar_7 + star_7);
+*/
+        }
+    }
+}
